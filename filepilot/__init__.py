500e5392d65b6c9e53e3ce24f4f9c87b6fe8b8f7
federicograssellino-cmd <federicograssellino@gmail.com>
2026-07-20 12:52:12 +0200

    Add files via upload

--- filepilot/__init__.py
+++ filepilot/__init__.py
@@ -4,4 +4,4 @@ Il numero di versione vive qui, in un solo posto, cosi' sia il README
 sia la scheda "Info" delle Impostazioni lo leggono dalla stessa fonte
 invece di rischiare di andare fuori sincrono tra loro.
 """
-__version__ = "0.44"
+__version__ = "0.47"
